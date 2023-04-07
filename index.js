b26dc22a8a47b72f01dca21a5b9356dc650dc815
feh-franc0 <fernandofv1110@gmail.com>
2023-04-06 21:22:10 -0300

    Collision blocks

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = 64 * 16
-canvas.height = 64 * 9
+canvas.width = 64 * 16;
+canvas.height = 64 * 9;
+
+const parsedCollisions = collisionsLevel1.parse2D();
+const collisionBlocks = parsedCollisions.createObjectsFrom2D();
 
 const backgroundLevel1 = new Sprite({
   position: {
     x: 0,
     y: 0,
   },
-  imageSrc: './img/backgroundLevel1.png'
-})
+  imageSrc: "./img/backgroundLevel1.png",
+});
 
-const player = new Player()
+const player = new Player();
 
 const keys = {
   w: {
@@ -24,20 +27,21 @@ const keys = {
   d: {
     pressed: false,
   },
-}
+};
 function animate() {
-  window.requestAnimationFrame(animate)
-  // c.fillStyle = 'white'
-  // c.fillRect(0, 0, canvas.width, canvas.height)
+  window.requestAnimationFrame(animate);
 
-  backgroundLevel1.draw()
+  backgroundLevel1.draw();
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.draw();
+  });
 
-  player.velocity.x = 0
-  if (keys.d.pressed) player.velocity.x = 5
-  else if (keys.a.pressed) player.velocity.x = -5
-  
-  player.draw()
-  player.update()
+  player.velocity.x = 0;
+  if (keys.d.pressed) player.velocity.x = 5;
+  else if (keys.a.pressed) player.velocity.x = -5;
+
+  player.draw();
+  player.update();
 }
 
-animate()
+animate();
